8784c4770c83e564e6d4c97387e5199a4a057c48
Горохова Анастасия Александровна <pr111gorohova@oat.local>
2024-11-13 17:25:46 +0600

    huita

--- app/src/main/java/com/example/smarthouse/Code.kt
+++ app/src/main/java/com/example/smarthouse/Code.kt
@@ -2,10 +2,55 @@ package com.example.smarthouse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.LinearLayout
 
 class Code : AppCompatActivity() {
+
+    private var numberCount = 0
+    private lateinit var dotsContainer: GridLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_code)
+
+
+
     }
+    public fun onClick(view: View){
+        val button : Button = findViewById(view.id)
+        val number = button.text
+        Log.e("!", number.toString())
+        numberCount ++
+
+/*        val dotView = View(this)
+        val params = LinearLayout.LayoutParams(20,20)
+        dotView.layoutParams=params
+        dotView.setBackgroundResource(R.drawable.dot)
+        dotsContainer.addView(dotView)*/
+
+/*        val button : Button = view.id
+        for (i in 1..9){
+            if (button.text == i.toString()){
+                addNumber(i)
+            }
+
+*//*            val buttonId = resources.getIdentifier("button_$i", "id", "packageName")
+            val button: Button = findViewById(buttonId)
+            button.setOnClickListener{addNumber(i)}*//*
+        }*/
+    }
+
+/*    private fun addNumber(number: Int) {
+        numberCount ++
+        val dotView = View(this)
+        val params = LinearLayout.LayoutParams(20,20)
+        dotView.layoutParams=params
+        dotView.setBackgroundResource(R.drawable.dot)
+        dotsContainer.addView(dotView)
+
+    }*/
 }
--- app/src/main/java/com/example/smarthouse/MainActivity.kt
+++ app/src/main/java/com/example/smarthouse/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.smarthouse
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button  = findViewById(R.id.mButton)
+        button.setOnClickListener{
+            val intent = Intent(this, Code::class.java)
+            startActivity(intent)
+        }
+
+        val button2 : Button = findViewById(R.id.buttonReg)
+        button2.setOnClickListener{
+            val intent2 = Intent(this, Registration::class.java)
+            startActivity(intent2)
+        }
+
     }
 }
